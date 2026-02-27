a678fafb5766c174398d6882588745fa8113d39b
Moises Blanca <moisesblanca@fourptzero.com>
2026-02-27 11:44:44 +0800

    For PR (ENV removed)

--- src/constants/clientDisplayMap.ts
+++
@@ -1,6 +0,0 @@
-export const CLIENT_DISPLAY_NAME_MAP: Record<string, string> = {
-    vlpay: "VLPay",
-};
-
-export const getDisplayNameFromUsername = (username: string) =>
-    CLIENT_DISPLAY_NAME_MAP[username] || username;
